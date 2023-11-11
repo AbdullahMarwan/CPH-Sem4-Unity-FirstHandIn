dbba305a55159258010fab50890ae2cd46e7e605
AbdullahMarwan <abdu.moussa01@gmail.com>
2023-11-11 13:45:59 +0100

    Improved PlayerMovement to make it more fluid

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,48 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloatyMcStinky : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
- [SerializeField] private Rigidbody rigidbody;
- [SerializeField] private float jumpForce = 10f;
- [SerializeField] private float forwardForce = 10f;
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float forwardForce = 10f;
+    [SerializeField] private float damping = 0.5f; // Adjust the damping factor as needed
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        // Freeze rotation on all axes
+        rigidbody.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigidbody.AddForce(Vector3.up * jumpForce);
-            
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
 
-        if (Input.GetKeyDown(KeyCode.D)) 
-        {
-            rigidbody.AddForce(Vector3.right * forwardForce);
-        }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.A)) 
-        {
-            rigidbody.AddForce(Vector3.left * forwardForce);
-        }
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
-        if (Input.GetKeyDown(KeyCode.W)) 
-        {
-            rigidbody.AddForce(Vector3.forward * forwardForce);
-        }
+        rigidbody.AddForce(moveDirection * forwardForce);
 
-        if (Input.GetKeyDown(KeyCode.S)) 
-        {
-            rigidbody.AddForce(Vector3.back * forwardForce);
-        }
-
-    
-        
+        // Apply damping to reduce velocity when no input is given
+        Vector3 dampingForce = -rigidbody.velocity * damping;
+        rigidbody.AddForce(dampingForce);
     }
 }
